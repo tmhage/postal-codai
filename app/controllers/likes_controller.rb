1f8081e8fcf70170fc93a33e262456c3f199d052
tmhage <attmhage@gmail.com>
2016-08-11 11:17:21 +0200

    toggle like posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,16 @@
 class LikesController < ApplicationController
 
   def create
-      post = Post.find( params[:post_id] )
-      like = Like.new( post: post, user: current_user )
+        post = Post.find( params[:post_id] )
 
-      like.save
+        if like = Like.find_by( post: post, user: current_user )
+           like.destroy
+        else
+           like = Like.new( post: post, user: current_user )
+           like.save
+        end
 
-      redirect_to posts_path
-   end
+        redirect_to posts_path
+     end
+
 end

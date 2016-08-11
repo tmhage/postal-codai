9d000e9468a2e97f67f80e6463bbaa9a2b3b1c11
tmhage <attmhage@gmail.com>
2016-08-11 10:23:05 +0200

    Seed posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.delete_all
+
+Post.create( content: "Pizza is yummy!"                    )
+Post.create( content: "Veggies are tasty and nutritious."  )
+Post.create( content: "Salt is the best ingredient."       )
+
+Post.create( content: "I've seen Star Wars 23 times."      )
+Post.create( content: "Walking Dead has an exciting plot." )
+Post.create( content: "I only watch art-house films."      )
+
+Post.create( content: "My cats are cute!"      )
+Post.create( content: "Dogs are good friends."  )
+Post.create( content: "My hamster resents me." )
+
+Post.create( content: "Free market solves all problems."         )
+Post.create( content: "Workers control the means of production." )
+Post.create( content: "Can't we all just get along?"             )

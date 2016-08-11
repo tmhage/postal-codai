4cc406ca509bfe17bb41a10db5fb147553f5742d
tmhage <attmhage@gmail.com>
2016-08-11 11:03:16 +0200

    Add user to posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,18 +13,18 @@ anton = User.create( email: 'anton@ex.com', password: 'abcd1234' )
 
 Post.delete_all
 
-Post.create( content: "Pizza is yummy!"                    )
-Post.create( content: "Veggies are tasty and nutritious."  )
-Post.create( content: "Salt is the best ingredient."       )
+Post.create( content: "Pizza is yummy!"                   , user: frank )
+Post.create( content: "Veggies are tasty and nutritious." , user: alice )
+Post.create( content: "Salt is the best ingredient."      , user: anton )
 
-Post.create( content: "I've seen Star Wars 23 times."      )
-Post.create( content: "Walking Dead has an exciting plot." )
-Post.create( content: "I only watch art-house films."      )
+Post.create( content: "I've seen Star Wars 23 times."      , user: frank )
+Post.create( content: "Walking Dead has an exciting plot." , user: alice )
+Post.create( content: "I only watch art-house films."      , user: anton )
 
-Post.create( content: "My cats are cute!"      )
-Post.create( content: "Dogs are good friends."  )
-Post.create( content: "My hamster resents me." )
+Post.create( content: "My cats are cute!"      , user: frank )
+Post.create( content: "Dogs are good friends." , user: alice )
+Post.create( content: "My hamster resents me." , user: anton )
 
-Post.create( content: "Free market solves all problems."         )
-Post.create( content: "Workers control the means of production." )
-Post.create( content: "Can't we all just get along?"             )
+Post.create( content: "Free market solves all problems."         , user: frank )
+Post.create( content: "Workers control the means of production." , user: alice )
+Post.create( content: "Can't we all just get along?"             , user: anton )

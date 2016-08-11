910ff2e1ad2a62b8cb7bfa5784db35d9d12bd1fb
tmhage <attmhage@gmail.com>
2016-08-11 10:46:04 +0200

    Order posts by creation date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
-   @posts = Post.all
-end
+       @posts = Post.order( created_at: :desc )
+    end
 
 def create
    post_params = params.require( :post ).permit( :content )
